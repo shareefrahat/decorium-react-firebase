8c285603925d3e146b4d3bca6c8a3de4f463b29f
ShareefRahat <imr4h47@gmail.com>
2022-04-02 23:20:57 +0600

    footer ui developed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-40 mx-0 bg-slate-900 text-gray-300">
+      <section></section>
+      <section className="flex flex-col md:flex-row justify-evenly items-center py-10">
+        <div>
+          <h5 className="text-md text-yellow-400 font-bold">PRODUCTS</h5>
+          <ul className="my-5">
+            <li>
+              <a href="a">Table Chair</a>
+            </li>
+            <li>
+              <a href="a">Book Shelves</a>
+            </li>
+            <li>
+              <a href="a">Sofa Set</a>
+            </li>
+            <li>
+              <a href="a">Bed and Decorative</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h5 className="text-md text-yellow-400 font-bold">STAKEHOLDERS</h5>
+          <ul className="my-5">
+            <li>
+              <a href="a">Google</a>
+            </li>
+            <li>
+              <a href="a">Amazon</a>
+            </li>
+            <li>
+              <a href="a">Tesla Ltd.</a>
+            </li>
+            <li>
+              <a href="a">Fesla Inc.</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h5 className="text-md text-yellow-400 font-bold">SPONSORS</h5>
+          <ul className="my-5">
+            <li>
+              <a href="a">Dell</a>
+            </li>
+            <li>
+              <a href="a">Samsung</a>
+            </li>
+            <li>
+              <a href="a">Xiaomi</a>
+            </li>
+            <li>
+              <a href="a">Code House</a>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden md:block border-r-2 h-20 border-r-slate-400"></div>
+        <div>
+          <h5 className="text-md text-yellow-400 font-bold">SUBSCRIPTION</h5>
+          <div className="my-5">
+            <h5 className="text-sm mb-2">Subscribe to our Newsletter</h5>
+            <div className="mb-4">
+              <input
+                className="px-3 py-2 bg-white text-black border  placeholder-slate-400 focus:outline-none focus:border-yellow-500  block w-full rounded-full sm:text-sm focus:ring-1"
+                type="email"
+                name=""
+                id=""
+                placeholder="Enter your email address"
+              />
+            </div>
+            <div>
+              <button className="bg-yellow-400 rounded-full px-5 py-1 text-black">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className=" py-8 text-md border-t border-t-slate-400">
+        <p>&copy; Copyright Reserved | Decorium 2022</p>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,10 @@ const Header = () => {
   return (
     <header className="bg-slate-900 text-white flex flex-col md:flex-row justify-around items-center px-2 md:px-0 py-5 md:py-8 m-0">
       <div className="flex flex-row justify-center items-center my-5 md:my-0">
-        <img className="w-10" src={logo} alt="" />
-        <h1 className="text-4xl mx-2 text-gray-100">Decorium</h1>
+        <img className="w-8" src={logo} alt="" />
+        <h1 className="text-4xl mx-2  text-yellow-400 hover:text-gray-100">
+          Decorium
+        </h1>
       </div>
       <div className="flex flex-row justify-center items-center my-5 md:my-0">
         <div>
@@ -20,17 +22,20 @@ const Header = () => {
             placeholder="Search your products"
           />
         </div>
-        <button className="block bg-yellow-400 rounded-full w-fit text-black ml-[-40px] p-2 border-2 border-yellow-400">
+        <button className="block bg-yellow-400 rounded-full w-fit text-black ml-[-40px] p-2 border-2 border-yellow-400 ">
           <SearchIcon className="w-5"></SearchIcon>
         </button>
       </div>
       <div className="flex flex-row justify-between items-center my-5 md:my-0">
         <button className="mx-5">
-          <UserCircleIcon className="w-8 text-gray-300"></UserCircleIcon>
+          <UserCircleIcon className="w-8 text-gray-300 hover:text-yellow-400"></UserCircleIcon>
         </button>
         <span className=" md:block text-slate-400">|</span>
-        <button className="mx-5">
-          <ShoppingCartIcon className="w-8 text-gray-300"></ShoppingCartIcon>
+        <button className="mx-5 relative">
+          <span className="bg-yellow-400 text-black aspect-square rounded-full absolute bottom-5 left-4 px-1 font-bold">
+            99
+          </span>
+          <ShoppingCartIcon className="w-8 text-gray-300 hover:text-yellow-400"></ShoppingCartIcon>
         </button>
       </div>
     </header>
